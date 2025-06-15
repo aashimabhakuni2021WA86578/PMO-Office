8593daa2c9fe3e17f449eba2124648c89e02b451
aashimabhakuni2021WA86578 <2021WA86578@wilp.bits-pilani.ac.in>
2025-06-15 22:03:06 +0530

    Delete src/org/javascript.java

--- src/org/javascript.java
+++
@@ -1,8 +0,0 @@
-package org.hello;
-import java.util.logging.Logger;
-public class JavaScript {
-    private static final Logger logger = Logger.getLogger(JavaScript.class.getName());
-    public static void main(String[] args) {
-        logger.info("Hello BITS from SonarQube analysis!");
-    }
-}
