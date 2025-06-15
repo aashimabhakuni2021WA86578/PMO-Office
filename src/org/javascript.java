59502539f17c9f56fabd0b51b07f0ed1acd8112e
aashimabhakuni2021WA86578 <2021WA86578@wilp.bits-pilani.ac.in>
2025-06-15 21:58:59 +0530

    Add files via upload

---
+++ src/org/javascript.java
@@ -0,0 +1,8 @@
+package org.hello;
+import java.util.logging.Logger;
+public class JavaScript {
+    private static final Logger logger = Logger.getLogger(JavaScript.class.getName());
+    public static void main(String[] args) {
+        logger.info("Hello BITS from SonarQube analysis!");
+    }
+}
